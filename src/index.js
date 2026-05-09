94b4a7d533a9c8bb0d46a797b3ab652081af76ca
petrudemian <petredemi@gmail.com>
2026-05-09 21:57:57 +0100

    activated cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/index.js';
-//import jwt from 'jsonwebtoken'
 
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const whitelist = ['https://european-cuisine.netlify.app',  'https://blogabout-cuisine.netlify.app'];
-app.use(cors())
-/*app.use(cors({
+//app.use(cors())
+app.use(cors({
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
@@ -16,66 +15,7 @@ app.use(cors())
             callback(new Error('Not allowed by CORS'));
         }
     }
-})); */
-/*
-app.get('/api', verifyToken, (req, res) =>{
-  //console.log(req.me)
-   jwt.verify(req.token, 'secretekey', (err, authData) =>{
-  //  console.log(authData)
-    res.setHeader( 'Content-Type', 'application/json')
-      if(err){
-      res.sendStatus(403)
-    }else{
-        res.json({message: 'welcomme to api'})
-      }
-  })
-})
-app.get('/api/posts',verifyToken, (req, res) =>{
-  jwt.verify(req.token, 'secretekey', (err, authData) =>{
-    console.log(authData)
-  // console.log(req.me)
-    if(err){
-      res.sendStatus(403)
-    }else{
-       res.json({
-        message: 'post created',
-       // authData
-        })
-    }
-  }) 
-})
-app.post('/api/login', (req, res) =>{
-  const user = {
-    id: 1,
-    username: 'brad',
-    email: 'brad@gmail.com'
-  }
-  jwt.sign({user}, 'secretekey', {expiresIn: '1h'}, (err, token) =>{
-  res.json({
-      token
-    })
-  })
-}) */
-// format of token
-//authorization : Bearer <access_token>
-/*
-function verifyToken(req, res, next){
-  //get autorisation token
-  const bearerHeader = req.headers['authorization']
-  // check if bearer is undefined
-  if (typeof bearerHeader !== 'undefined'){
-    //split at the space
-    const bearer = bearerHeader.split(' ')
-    const bearerToken = bearer[1]
-    //console.log(bearerToken)
-    req.token = bearerToken
-    next()
-
-  }else {
-    //forbidden
-    res.sendStatus(403)
-  } 
-} */
+})); 
 app.use('/',routes.firstpage)
 app.use('/user', routes.user)
 app.use('/message', routes.message)
