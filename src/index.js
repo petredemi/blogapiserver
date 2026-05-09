dedd1f92b68b87ecece39a2cfab6cb47912bcb2f
petrudemian <petredemi@gmail.com>
2026-05-09 20:08:17 +0100

    introduce cgeneral cores

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const whitelist = ['https://european-cuisine.netlify.app',  'https://blogabout-cuisine.netlify.app'];
+app.use(cors())
 /*app.use(cors({
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
